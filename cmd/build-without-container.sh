9eb5923866432eef10513646c6ad8491fd7b5b42
Novs Yama <frost-tb-voo@users.noreply.github.com>
2020-04-27 15:48:14 +0900

    Fix the bash script to change the directory to specified one.

--- cmd/build-without-container.sh
+++ cmd/build-without-container.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
+DIR=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
+cd ${DIR}
+
 sudo apt-get update
 sudo apt-get install -y ffmpeg
 dpkg --get-selections | grep codec
 
+# go get github.com/gdamore/tcell
+# go get github.com/nsf/termbox-go
 go build .
-
+mv cmd ffplay-cli
+chmod +x ffplay-cli
